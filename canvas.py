1127e4f0cbbe79bc384d191d67413c4cfbc7084b
Saurav Kumar Rathaur <71010336+Sauravkrrathaur99@users.noreply.github.com>
2022-06-12 23:19:51 +0530

    Canvas: It is used to draw pictures and other complex layout like graphics, text and widgets.

---
+++ canvas.py
@@ -0,0 +1,23 @@
+# The general syntax is:
+# w = Canvas(master, option=value)
+# master is the parameter used to represent the parent window.
+# There are number of options which are used to change the format of the widget. Number of options can be passed as parameters separated by commas. Some of them are listed below.
+#
+# bd: to set the border width in pixels.
+# bg: to set the normal background color.
+# cursor: to set the cursor used in the canvas.
+# highlightcolor: to set the color shown in the focus highlight.
+# width: to set the width of the widget.
+# height: to set the height of the widget.
+
+from tkinter import *
+master = Tk()
+w = Canvas(master, width=100, height=100)
+w.pack()
+canvas_height=50
+canvas_width=200
+y = int(canvas_height / 2)
+w.create_line(0, y, canvas_width, y )
+mainloop()
+
+
